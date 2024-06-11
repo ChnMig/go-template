0b495383ab0d82bc1e4a0a547a314945e6759886
chnmig <chnmig@outlook.com>
2024-06-11 18:42:11 +0800

    🤔 add sign stop

--- http-server/main.go
+++ http-server/main.go
@@ -3,6 +3,11 @@ package main
 import (
 	"http-server/util/log"
 	runmodel "http-server/util/run-model"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"go.uber.org/zap"
 )
 
 func main() {
@@ -10,4 +15,16 @@ func main() {
 	log.GetLogger()
 	// Set the running mode of the program
 	runmodel.Detection()
+	// End of monitoring
+	func() {
+		sigs := make(chan os.Signal, 1)
+		signal.Notify(sigs, syscall.SIGTERM, syscall.SIGINT)
+		for {
+			switch <-sigs {
+			case syscall.SIGTERM, syscall.SIGINT:
+				zap.L().Error("i picked up a stop signal.")
+				return
+			}
+		}
+	}()
 }
