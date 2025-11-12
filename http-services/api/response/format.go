5f37c3ec694293cedc68656272413612c29b438d
chenming <chnmig@outlook.com>
2025-11-12 14:29:18 +0800

    feat: 在响应函数中添加日志记录，增强错误和成功响应的可追踪性

--- http-services/api/response/format.go
+++ http-services/api/response/format.go
@@ -1,6 +1,7 @@
 package response
 
 import (
+	"http-services/utils/log"
 	"net/http"
 	"time"
 
@@ -18,55 +19,65 @@ func getTraceID(c *gin.Context) string {
 }
 
 func ReturnErrorWithData(c *gin.Context, data responseData, result interface{}) {
+	l := log.FromContext(c)
 	data.Timestamp = time.Now().Unix()
 	data.TraceID = getTraceID(c)
 	data.Detail = result
 	c.JSON(http.StatusOK, data)
+	l.Info("Returning error response with data")
 	// Return directly
 	c.Abort()
 }
 
 // ResponseOk
 func ReturnOk(c *gin.Context, result interface{}) {
+	l := log.FromContext(c)
 	data := OK
 	data.Timestamp = time.Now().Unix()
 	data.TraceID = getTraceID(c)
 	data.Detail = result
 	c.JSON(http.StatusOK, data)
+	l.Info("Returning OK response")
 	// Return directly
 	c.Abort()
 }
 
 // ResponseOkWithTotal
 func ReturnOkWithTotal(c *gin.Context, total int, result interface{}) {
+	l := log.FromContext(c)
 	data := OK
 	data.Timestamp = time.Now().Unix()
 	data.TraceID = getTraceID(c)
 	data.Detail = result
 	data.Total = &total
 	c.JSON(http.StatusOK, data)
+	l.Info("Returning OK response with total")
 	// Return directly
 	c.Abort()
 }
 
 // ResponseError
 func ReturnError(c *gin.Context, data responseData, message string) {
+	l := log.FromContext(c)
 	data.Timestamp = time.Now().Unix()
 	data.TraceID = getTraceID(c)
 	if message != "" {
 		data.Message = message
 	}
 	c.JSON(http.StatusOK, data)
+	l.Info("Returning error response")
 	// Return directly
 	c.Abort()
 }
 
 // ResponseSuccess
 func ReturnSuccess(c *gin.Context) {
+	l := log.FromContext(c)
 	data := OK
 	data.Timestamp = time.Now().Unix()
 	data.TraceID = getTraceID(c)
 	c.JSON(http.StatusOK, data)
+	l.Info("Returning success response")
 	// Return directly
 	c.Abort()
 }
